73496a59a57cb677fb958dce73795912743bf6c8
dungnt <dungnt@vivas.vn>
2023-02-27 21:00:57 +0700

    add worker to service

--- gBase/result.go
+++ gBase/result.go
@@ -5,9 +5,9 @@ import "gitlab.vivas.vn/go/grpc_api/api"
 type ContextType uint8
 
 const (
-	ContextType_BIN   ContextType = 0x20
-	ContextType_JSON  ContextType = 0x40
-	ContextType_PROTO ContextType = 0x60
+	ContextType_BIN   ContextType = 0x2
+	ContextType_JSON  ContextType = 0x4
+	ContextType_PROTO ContextType = 0x8
 	ContextType_NONE  ContextType = 0x0
 )
 
