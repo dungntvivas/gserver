dafc3cedcdc8fd49361d0c815b1dd2972b8ce9d1
Dũng Nguyễn <dungnt@Dungs-Mac-mini.local>
2023-02-28 14:01:25 +0700

    update

--- gService/gService.go
+++ gService/gService.go
@@ -6,17 +6,22 @@ import (
 	"gitlab.vivas.vn/go/gserver/gHTTP"
 	"gitlab.vivas.vn/go/gserver/gRPC"
 	"gitlab.vivas.vn/go/internal/logger"
+	"google.golang.org/protobuf/encoding/protojson"
+	"google.golang.org/protobuf/proto"
 	"os"
 	"os/signal"
+	"runtime"
 )
 
 type CallbackRequest func(*gBase.Payload)
+type CallbackApiRequest func(request *api.Request,reply chan *api.Reply,v_auth string) chan int
 type GService struct {
 	done           *chan struct{}
 	interrupt      chan os.Signal
 	receiveRequest chan *gBase.Payload
 	Logger         *logger.Logger
-	cb             CallbackRequest
+	rawcb             CallbackRequest
+	cb CallbackApiRequest
 	SvName         string `default:"Service"`
 
 	http_server *gHTTP.HTTPServer
@@ -50,6 +55,55 @@ func (p *GService) Wait() {
 	<-*p.done
 }
 
+func (p *GService)runner(){
+    loop:
+    	for{
+			select {
+			case <-*p.done:
+				break loop
+			case j := <- p.receiveRequest:
+				if p.cb != nil {
+					request := api.Request{}
+					chreply := make(chan *api.Reply)
+					if err := proto.Unmarshal(j.Request.BinRequest, &request); err != nil {
+						j.ChResult <- &gBase.Result{Status: int(api.ResultType_REQUEST_INVALID), Reply: &api.Reply{Status: uint32(api.ResultType_REQUEST_INVALID)}}
+						goto loop
+					}
+					p.cb(&request,chreply,j.V_Authorization)
+					reply := <- chreply
+
+					if(reply.Status >= 1000){
+						reply.Msg = api.ResultType(reply.Status).String()
+					}
+					//convert reply to json or binary
+					if j.Request.PayloadType == uint32(gBase.ContextType_BIN){
+						b, err := proto.Marshal(reply)
+						if err != nil {
+							j.ChResult <- &gBase.Result{Status: int(api.ResultType_INTERNAL_SERVER_ERROR), Reply: &api.Reply{Status: uint32(api.ResultType_INTERNAL_SERVER_ERROR),Msg: "INTERNAL_SERVER_ERROR"}}
+							goto loop
+						}
+						j.ChResult <- &gBase.Result{Status: int(reply.Status), Reply: reply,ReplyData: b,ContextType: gBase.ContextType(j.Request.PayloadType)}
+					}else if j.Request.PayloadType == uint32(gBase.ContextType_JSON){
+						jsonBytes, err := protojson.Marshal(reply)
+						if err != nil {
+							j.ChResult <- &gBase.Result{Status: int(api.ResultType_INTERNAL_SERVER_ERROR), Reply: &api.Reply{Status: uint32(api.ResultType_INTERNAL_SERVER_ERROR)}}
+							goto loop
+						}
+						j.ChResult <- &gBase.Result{Status: int(reply.Status), Reply: reply,ReplyData: jsonBytes,ContextType: gBase.ContextType(j.Request.PayloadType)}
+					}else if j.Request.PayloadType == uint32(gBase.ContextType_PROTO){ // return reply only
+						j.ChResult <- &gBase.Result{Status: int(reply.Status), Reply: reply,ContextType: gBase.ContextType(j.Request.PayloadType)}
+					}else{
+						j.ChResult <- &gBase.Result{Status: int(api.ResultType_STRANGE_REQUEST), Reply: &api.Reply{Status: uint32(api.ResultType_STRANGE_REQUEST),Msg: "STRANGE_REQUEST"}}
+					}
+				}else if p.rawcb != nil {
+					p.rawcb(j) // thường sử dụng cho gateway vì ko đụng chạm gì đến request mà chuyển tiếp trực tiếp
+				} else {
+					j.ChResult <- &gBase.Result{Status: int(api.ResultType_STRANGE_REQUEST), Reply: &api.Reply{Status: uint32(api.ResultType_STRANGE_REQUEST),Msg: "STRANGE_REQUEST"}}
+				}
+			}
+		}
+}
+
 func (p *GService) StartListenAndReceiveRequest() chan struct{} {
 
 	if p.http_server != nil {
@@ -58,6 +112,10 @@ func (p *GService) StartListenAndReceiveRequest() chan struct{} {
 
 	if p.grpc_server != nil {
 		p.grpc_server.Serve()
+	}
+
+	for i := 0;i<runtime.NumCPU()*2;i++ {
+		go p.runner()
 	}
 
 	go func() {
@@ -69,12 +127,6 @@ func (p *GService) StartListenAndReceiveRequest() chan struct{} {
 			case <-p.interrupt:
 				p.LogInfo("shutting down gracefully")
 				break loop
-			case j := <-p.receiveRequest:
-				if p.cb != nil {
-					p.cb(j)
-				} else {
-					j.ChResult <- &gBase.Result{Status: int(api.ResultType_STRANGE_REQUEST), Reply: &api.Reply{Status: uint32(api.ResultType_STRANGE_REQUEST)}}
-				}
 			}
 		}
 
@@ -96,7 +148,13 @@ func (p *GService) StartListenAndReceiveRequest() chan struct{} {
 	return *p.done
 }
 
-func (p *GService) RegisterHandler(request CallbackRequest) {
+// RegisterHandlerRawRequest sử dụng khi muốn service call raw request từ server
+func (p *GService) RegisterHandlerRawRequest(request CallbackRequest) {
+	p.rawcb = request
+}
+
+// RegisterHandlerRequest sử dụng khi muốn service chuyển đổi payload từ raw sang api.request
+func (p *GService) RegisterHandlerRequest(request CallbackApiRequest) {
 	p.cb = request
 }
 
