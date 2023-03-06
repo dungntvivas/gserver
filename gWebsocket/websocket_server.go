62289584c094a2f34e63e211c3ee4e05f1df64cb
Dũng Nguyễn <dungnt@Dungs-Mac-mini.local>
2023-03-06 14:09:07 +0700

    edit ws server

--- gWebsocket/websocket_server.go
+++ gWebsocket/websocket_server.go
@@ -1,33 +1,15 @@
 package gWebsocket
 
 import (
-	"gitlab.vivas.vn/go/grpc_api/api"
 	"gitlab.vivas.vn/go/gserver/gBase"
-	"google.golang.org/grpc"
-	"net"
 )
 
 type WSServer struct {
-	gBase.GServer
-	lis net.Listener
-	s *grpc.Server
-	api.UnimplementedAPIServer
+	gBase.SocketServer
 }
 func New(config gBase.ConfigOption, chReceiveRequest chan *gBase.Payload) *WSServer {
-
-	b := gBase.GServer{
-		Config:           &config,
-		ChReceiveRequest: chReceiveRequest,
-	}
 	p := &WSServer{
-		GServer: b,
+		gBase.NewSocket(config,chReceiveRequest),
 	}
-	if p.Config.Tls.IsTLS {
-		p.Config.ServerName = "WSS"
-	} else {
-		p.Config.ServerName = "WS"
-	}
-
-
-	return p
+	return  p
 }
