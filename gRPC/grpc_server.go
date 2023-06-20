cc147b015530a3ea595ed7f7184433c661c4d6fd
Dũng Nguyễn <dungnt@Dungs-Mac-mini-5.local>
2023-06-20 17:07:54 +0700

    update api version

--- gRPC/grpc_server.go
+++ gRPC/grpc_server.go
@@ -91,6 +91,7 @@ func (p *GRPCServer) Close() {
 
 func (p *GRPCServer) SendRequest(ctx context.Context, request *api.Request) (*api.Reply, error) {
 	chReply := make(chan *api.Reply)
+	request.PayloadType = uint32(gBase.PayloadType_BIN)
 	// send data to handler
 	p.HandlerRequest(&gBase.Payload{Request: request, ChReply: chReply})
 	// wait for return data
